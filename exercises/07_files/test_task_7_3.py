85a88ecd52b136d861e13524ff1d7ebeeac8112a
Baldeurik <baldeurik@gmail.com>
2024-11-07 12:57:25 +0300

    rework 7.3

--- exercises/07_files/test_task_7_3.py
+++ exercises/07_files/test_task_7_3.py
@@ -17,12 +17,12 @@ def test_task_stdout(capsys):
     correct_stdout = unified_columns_output(
         "100      01bb.c580.7000      Gi0/1\n"
         "200      0a4b.c380.7c00      Gi0/2\n"
-        "200      1a4b.c580.7000      Gi0/6\n"
         "300      a2ab.c5a0.700e      Gi0/3\n"
-        "300      0a1b.5c80.70f0      Gi0/7\n"
         "10       0a1b.1c80.7000      Gi0/4\n"
-        "10       01ab.c5d0.70d0      Gi0/8\n"
         "500      02b1.3c80.7b00      Gi0/5\n"
+        "200      1a4b.c580.7000      Gi0/6\n"
+        "300      0a1b.5c80.70f0      Gi0/7\n"
+        "10       01ab.c5d0.70d0      Gi0/8\n"
         "1000     0a4b.c380.7d00      Gi0/9"
     )
     assert (
